66e17691eb4b744f60e40483a71442d2d094b78d
Segolene de la Taille <segolette@hotmail.com>
2020-11-30 17:52:18 +0100

    ça avance, ça avance...

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,43 +1,59 @@
 class RecipesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
-  include Wicked::Wizard
-
-  steps :add_ingredient, :add_details
 
   def index
    @recipes = policy_scope(Recipe).where(user_id:current_user)
   end
 
   def show
-    @recipe = Recipe.find(params[:recipe_id])
+    @recipe = Recipe.find(params[:id])
     authorize @recipe
-    render_wizard
   end
 
   def new
     @recipe = Recipe.new
     authorize @recipe
-    render_wizard
   end
 
-  def create
+   def create
     @recipe = Recipe.new(recipe_params)
     @recipe.user = current_user
     authorize @recipe
+    new_measure = Measure.new
+    new_measure.recipe = @recipe
+    new_measure.save
+    #raise
+    # création des mesures
+    #new_measure_params = params.select{ |p| p.include?('new_measure_id') }
+    # recipe_params.each do |new_measure_param|
+    #   new_measure = Measure.new
+    #   new_measure.recipe = @recipe
+    #   #raise
+    #   # new_measure.ingredient_id = new_measure_param[1]
+
+    #   # new_measure_number = new_measure_param[0].split('_').last
+    #   # new_measure.quantity = params["new_measure_quantity_#{new_measure_number}"]
+    #   # new_measure.unit_id = new_measure_param[1]
+
+    #   # params_required = params["new_measure_required_#{new_measure_number}"]
+    #   # if params_required
+    #   #   new_measure.required = true
+    #   # else
+    #   #   new_measure.required = false
+    #   # end
+    #   new_measure.save
+    # end
+
     if @recipe.save
-      redirect_to wizard_path(steps.first, recipe_id: @recipe.id)
+      redirect_to recipe_create_recipe_sb_path(:addDescription, recipe_id: @recipe.id)
     else
       render :new
     end
   end
 
-  def update
-    @recipe = Recipe.find(params[:id])
-    @recipe.user = current_user
-    authorize @recipe
-    @recipe.update_attributes(params[:recipe_params])
-    render_wizard @recipe
-  end
+
+#
+#     end
 
   def edit
     @recipe = Recipe.find(params[:id])
@@ -47,9 +63,7 @@ class RecipesController < ApplicationController
 private
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description, :category_id, :usage)
+    params.require(:recipe).permit(:title, :category_id, :id, measures_attributes: [:id, :quantity, :recipe_id, :unit_id, :optionnal, :ingredient_id])
   end
-
-  # , photos: [], measures_attributes: [:id, :quantity, :recipe_id, :unit_id, :optionnal, :ingredient_id]
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :recipes do
+    resources :create_recipe_sbs, only: [ :show, :update ], controller: 'create_recipe_sbs'
     resources :measures, only: [ :new, :create, :destroy ]
   end
 
