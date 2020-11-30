0680ada324386e8fccbda12d04f1091e7ee04ca4
christel dufrasne <chbalsacq@gmail.com>
2020-11-30 11:25:02 +0100

    searchbar for home with simple sql_query

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,5 +7,15 @@ class Recipe < ApplicationRecord
   accepts_nested_attributes_for :measures
 
   #validates :title, :description, presence: true
-
+if params[:query].present?
+      sql_query = " \
+        recipes.title @@ :query \
+        OR recipes.description @@ :query \
+        OR categories.name @@ :query \
+      "
+      @recipes = Recipe.joins(:category).where(sql_query, query: "%#{params[:query]}%")
+    else
+      @movies = Recipe.all
+    end
+  end
 end
