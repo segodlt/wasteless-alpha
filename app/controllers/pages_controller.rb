b6fcd3d47b59ee3020f43d9592c82f25d632d448
Segolene de la Taille <segolette@hotmail.com>
2021-01-20 17:04:09 +0100

    homepage, un peu de front

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,31 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home, :dashboard]
 
   def home
+    @categories = Category.all
+    @category = Category.find_by(id:params[:category_id])
+    @recipes = Recipe.order(created_at: :desc)
+    if params[:category_id].present?
+      @recipes = @recipes.where(category_id:params[:category_id])
+    end
+
+    if params[:query].present?
+      sql_query = " \
+      recipes.title ILIKE :query \
+      OR recipes.description ILIKE :query \
+      OR categories.name ILIKE :query \
+      OR ingredients.name ILIKE :query \
+      "
+      @recipes = @recipes.joins({measures: :ingredient}).joins(:category).where(sql_query, query:"%#{params[:query]}%").uniq
+      # @recipes.
+    end
   end
+
+  # def dashboard
+  #   @user = current_user
+  # end
+
+  # def account
+  #   @user = current_user
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :recipes
+  has_one_attached :photo, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
